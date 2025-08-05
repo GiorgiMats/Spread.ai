1b43e695ae4477afe478027c64602760bf74f7f7
Giorgi Matsukatovi <giorgimatsukatov@gmail.com>
2025-08-06 01:30:25 +0400

    dawdi

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -12,3 +12,24 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+
+var faqCategories = ['Getting Started & Basic Information', 'Revenue Generation & Staking', 'Token Burns & Deflationary Mechanics', 'Premium Features & AI Access', 'Technology & Transparency']
+var activeFaqCategory = faqCategories[activeFaqNum];
+var activeFaqNum = 0;
+
+function changeFaqCategory(num) {
+    activeFaqNum = num;
+    document.querySelector('.fn-li-active').classList.remove("fn-li-active");
+    document.querySelector(`#fn-li-${num}`).classList.add('fn-li-active');
+
+    let faqItem = document.querySelectorAll('.faq-accordion');
+
+    for(i=0; i < faqItem.length; i++) {
+        if(faqItem[i].classList.contains(`faq-tree-${activeFaqNum}`)) {
+            faqItem[i].classList.remove('hidden');
+        } else {
+            faqItem[i].classList.add('hidden');
+        }
+    }
+}
